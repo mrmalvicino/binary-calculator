9c8506ce349185d86b489e5719d7408286cbc1ee
mrmalvicino <mrmalvicino@gmail.com>
2023-04-08 19:32:14 -0300

    Test basis_to_basis()

--- main.cpp
+++ main.cpp
@@ -58,32 +58,57 @@ int* decimal_to_basis(int num_dec, int out_basis){
     return num_b;
 }
 
-/*
-int* convert_basis(int num_b[], int in_basis, int out_basis){
-    return 0;
+
+void convert_basis(int num_b[], int in_basis, int out_basis){
+        int num_dec = basis_to_decimal(num_b, in_basis);
+        int size = get_digits(num_dec, out_basis);
+        for(int i = 0; i < size; i = i + 1){
+            cout << decimal_to_basis(num_dec, out_basis)[i];
+        }
+        cout << endl;
+
+        delete[] decimal_to_basis(num_dec, out_basis);
+    return;
 }
-*/
+
 
 int main(){
+    
     // Basis to decimal test:
+    cout << endl;
+    cout << "Basis to decimal test:" << endl;
+
     int num_b[] = {1,0,0,0,1,0,0,0};
     int in_basis = 2;
+
     cout << basis_to_decimal(num_b, in_basis) << endl;
 
     // Decimal to basis test:
+    cout << endl;
+    cout << "Decimal to basis test:" << endl;
+
     int num_dec = 136;
     int out_basis = 2;
-    int size = get_digits(num_dec, out_basis);
 
+    int size = get_digits(num_dec, out_basis);
     for(int i = 0; i < size; i = i + 1){
         cout << decimal_to_basis(num_dec, out_basis)[i];
     }
-
     cout << endl;
 
-    // Basis to basis test:
-
     delete[] decimal_to_basis(num_dec, out_basis);
+    
+
+    // Basis to basis test:
+    cout << endl;
+    cout << "Basis to basis test:" << endl;
+
+    int number[] = {1,0,0,0,1,0,0,0};
+    int input_basis = 2;
+    int output_basis = 10;
+
+    convert_basis(number, input_basis, output_basis);
+    cout << endl;
 
     return 0;
 }
