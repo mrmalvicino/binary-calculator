3a6a47da1cf7913146335d65b53ff466a7c8be31
mrmalvicino <mrmalvicino@gmail.com>
2023-04-08 18:09:46 -0300

    Test decimal_to_basis()

--- main.cpp
+++ main.cpp
@@ -4,46 +4,82 @@
 
 using namespace std;
 
-int main(){
-    int num_dec = 234, basis = 2, digits = 0;
 
+int get_digits(int num_dec, int out_basis){
+    /*
+    Determine how many digits are needed to represent a give decimal number in a certain basis.
+    */
 
-    // Determine how many digits are needed to represent num_dec in a given basis
-
-    int value = pow(basis, digits);
+    int digits = 0;
+    int value = pow(out_basis, digits);
     
     while(value < num_dec){
         digits = digits + 1;
-        value = pow(basis, digits);
+        value = pow(out_basis, digits);
     }
 
-    cout << digits << " digits are needed." << endl;
+    return digits;
+}
 
 
-    // Define an array of zeros with the calculated ammount of digits
-    
-    int num_b[digits];
+/*
+int basis_to_decimal(int* num_b[], int in_basis){
+    // Converts a given number in certain basis to decimal.
+
+    int digits = sizeof(num_b) / sizeof(num_b[0]);
+    int num_dec = 0;
 
     for(int i = 0; i < digits; i = i + 1){
+        num_dec = num_dec + num_b[digits-1-i] * pow(in_basis, i);
+    }
+
+    cout << "decimal: " << num_dec << endl;
+}
+*/
+
+
+int* decimal_to_basis(int num_dec, int out_basis){
+    /*
+    Converts a decimal number to a certain basis.
+    */
+
+    // Define an array of zeros:
+    int size = get_digits(num_dec, out_basis);
+    int* num_b = new int[size];
+
+    for(int i = 0; i < size; i = i + 1){
         num_b[i] = 0;
-        cout << num_b[i];
     }
 
-    cout << " array of zeros defined." << endl;
-
-
-    // Replace the array bits with ones to form the number
-
-    int aux = num_dec;
-
-    for(int i = digits - 1; i >= 0; i = i - 1){
-        num_b[i] = aux % basis;
-        aux = aux / basis;
+    // Replace the array bits with ones:
+    for(int i = size - 1; i >= 0; i = i - 1){
+        num_b[i] = num_dec % out_basis;
+        num_dec = num_dec / out_basis;
     }
 
-    cout << num_dec << " = ";
+    return num_b;
+}
 
-    for(int i = 0; i < digits; i = i + 1){
-        cout << num_b[i];
+
+int main(){
+    // Basis to decimal test:
+    //int num_b[] = {1,0,0,0,1,0,0,0};
+    //int in_basis = 2;
+    //cout << basis_to_decimal(num_b, in_basis) << endl;
+
+    // Decimal to basis test:
+    int num_dec = 136;
+    int out_basis = 2;
+    int size = get_digits(num_dec, out_basis);
+
+    for(int i = 0; i < size; i = i + 1){
+        cout << decimal_to_basis(num_dec, out_basis)[i];
     }
+
+    cout << endl;
+
+    // Basis to basis test:
+
+    delete[] decimal_to_basis(num_dec, out_basis);
+    return 0;
 }
